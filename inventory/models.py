c4bac1d5acc6b9252841488aabb9e45fd4c0a6e3
egecalay <egecalay@gmail.com>
2023-04-19 03:11:55 +0200

    created & registered Ingredient model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    AvailableQty    =models.IntegerField()
+    Ingredient      =models.TextField()
+    Unit            =models.TextField()
+    PricePerUnit    =models.DecimalField(decimal_places=2,max_digits=10)
