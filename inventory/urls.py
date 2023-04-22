6d82c5b8ec63404dd0c5b6a37cd8a2461a69e62b
egecalay <egecalay@gmail.com>
2023-04-23 01:49:00 +0200

    added list templates

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('inventory/ingredient/add', views.IngredientCreate.as_view(),name='ingredientcreate'),
     path('inventory/ingredient/<pk>/change', views.IngredientUpdate.as_view(),name='ingredientupdate'),
     path('inventory/ingredient/<pk>/delete', views.IngredientDelete.as_view(),name='ingredientdelete'),
+    path('inventory/menu/list', views.MenuList.as_view(),name='menulist'),
+    path('inventory/purchase/list', views.PurchaseList.as_view(),name='purchaselist'),
+
+
 
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,30 +1,41 @@
 from django.shortcuts import render
 # from django.http import HttpResponse # not used
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-from .models import Ingredient
+from .models import Ingredient, MenuItem, Purchase
 from .forms import IngredientCreateForm, IngredientUpdateForm
 
 # Create your views here.
 
 def home(request):
-    return render(request,'inventory/home.html')
+    context = {"name": request.user}
+    return render(request,'inventory/home.html',context)
 
+
+# CRUD- read
 class IngredientList(ListView):
     model = Ingredient
-    
 
+class MenuList(ListView):
+    model = MenuItem
+
+class PurchaseList(ListView):
+    model = Purchase    
+    
+# CRUD- create
 class IngredientCreate(CreateView):
     model = Ingredient
     form_class = IngredientCreateForm
     template_name = 'inventory/ingredient_add.html' 
 
+# CRUD- update
 class IngredientUpdate(UpdateView):
     model = Ingredient
     form_class = IngredientUpdateForm
     template_name = 'inventory/ingredient_update.html'
 
+# CRUD- delete
 class IngredientDelete(DeleteView):
     model = Ingredient
     template_name = 'inventory/ingredient_delete.html'
